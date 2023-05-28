c7d5311c27ccb78a6304682ca2a44116f8df1a00
Agshin Mammadov <107038254+agshinmammadov@users.noreply.github.com>
2023-05-28 13:34:13 +0400

    Github link added

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -16,7 +16,7 @@ const Header = () => {
                     </a>
                 </li>
                 <li className="w-[30px]">
-                    <a href="#">
+                    <a href="https://github.com/agshinmammadov/speechtotext.git">
                         <img src={Githubicon} alt="Github" />
                     </a>
                 </li>
